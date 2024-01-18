1558f9a91edbc5d3deaf9f3c78c3561490abdbb2
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-01-18 22:12:16 +0100

    Streaming GUI styling

--- src/gui/components/stream.cpp
+++ src/gui/components/stream.cpp
@@ -1,42 +1,53 @@
 #include "../gui_controller.hpp"
 
+#include "../extensions.hpp"
+
 
 namespace gui
 {
 	void GUIController::renderStream()
 	{
-		//ImGui::Text("Simulation in progress");
-
-		if (ImGui::Button("Abort simulation"))
-			streamManager.abortSimulation();
-
+		
+		
 		switch (streamManager.streamStatusGet())
 		{
 		case streaming::StreamStatus::initStatus:
-			ImGui::Text("Stream is getting ready");
+			ext::CenteredText("Stream initiates");
 			break;
 
 		case streaming::StreamStatus::waitingForServer:
-			ImGui::Text("Connecting to server...");
+			ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255,210,0,255)); // Yellow text
+			ext::CenteredText("Connecting to server...");
+			ImGui::PopStyleColor();
 			break;
 
 		case streaming::StreamStatus::inProgress:
-			ImGui::Text("Simulation in progress");
+			ext::CenteredText("Simulation in progress");
 			break;
 
 		case streaming::StreamStatus::successfullyEnded:
-			ImGui::Text("Simulation successfully ended");
+			ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(25,200,25,255)); // Green text
+			ext::CenteredText("Simulation successfully ended");
+			ImGui::PopStyleColor();
 			break;
 
 		case streaming::StreamStatus::connectionLost:
-			ImGui::Text("Connection with server lost!");
+			ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(235, 64, 52,255)); // Red text
+			ext::CenteredText("Connection with server lost!!!");
+			ImGui::PopStyleColor();
 			break;
 		
 		default:
 			break;
 		}
 
-		if (ImGui::Button("Stop watching stream"))
+		ImGui::NewLine();
+		if (ext::CenteredButton("Abort simulation"))
+			streamManager.abortSimulation();
+
+		
+		ImGui::NewLine();
+		if (ext::CenteredButton("Stop watching stream"))
 		{
 			setMode(Mode::mainScreen);
 			glController.setMode(graphics::Mode::None);
