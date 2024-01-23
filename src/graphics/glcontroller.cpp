613c4b65284d5bb1c2ead262361bb3831742eaac
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-01-23 02:06:29 +0100

    Addjust video config

--- src/graphics/glcontroller.cpp
+++ src/graphics/glcontroller.cpp
@@ -138,8 +138,8 @@ namespace graphics
 		int windowHeight;
 		glfwGetWindowSize(window, &windowWidth, &windowHeight);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame->getWidth(), frame->getHeight(),
-						0, GL_RGBA, GL_UNSIGNED_BYTE, frame->getData());
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame->getWidth(), frame->getHeight(),
+						0, GL_RGB, GL_UNSIGNED_BYTE, frame->getData());
 
 		glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                         GL_TEXTURE_2D, streamTex, 0);
